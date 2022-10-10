4555b9946531dcb38746cbd7213b01285e52708d
JeffreyHSE <Jeffrey.Ejeanye@hoffmann-group.com>
2022-10-10 16:38:57 +0200

    created Parameter class to return 2 parameters.

---
+++ src/de/fizzbuzz/main/game/Parameter.java
@@ -0,0 +1,16 @@
+package de.fizzbuzz.main.game;
+
+public class Parameter {
+
+    public int parameterThree;
+
+    public int parameterFive;
+
+
+    public Parameter(int parameterThree, int parameterFive) {
+        this.parameterFive = parameterFive;
+        this.parameterThree = parameterThree;
+    }
+
+
+}
