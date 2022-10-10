62218c5cd84929071d67f722bf47e2b1bfe7891e
JeffreyHSE <Jeffrey.Ejeanye@hoffmann-group.com>
2022-10-10 14:50:59 +0200

    added 2 boolean variables to prevent spamming multiple answers

--- src/de/fizzbuzz/main/game/FizzBuzz.java
+++ src/de/fizzbuzz/main/game/FizzBuzz.java
@@ -9,6 +9,10 @@ public class FizzBuzz {
     private final Scanner scanner = new Scanner(System.in);
     private int parameter = 0;
 
+    private boolean toHighChecker = true;
+
+    private boolean checkIfBoth = true;
+
 
     public void run() {
         this.gameLogic();
@@ -19,28 +23,36 @@ public class FizzBuzz {
         parameter = Integer.parseInt(scanner.nextLine());
         if (parameter > 100 || parameter < 0) {
             System.out.println("Die Nummer "+TerminalColors.ANSI_YELLOW + parameter +TerminalColors.ANSI_RESET+ " liegt nicht zwischen"+TerminalColors.ANSI_RED+" 1-100"+TerminalColors.ANSI_RESET+"!");
+            toHighChecker = false;
         }
     }
 
     private void CheckIfParameterCanBeDividedByThree() {
+        if (!checkIfBoth) return;
+        if (!toHighChecker) return;
         if (parameter % 3 == 0) {
             System.out.println("Fizz");
         }
     }
 
     private void CheckIfParameterCanBeDividedByFive() {
+        if (!checkIfBoth) return;
+        if (!toHighChecker) return;
         if (parameter % 5 == 0) {
             System.out.println("Buzz");
         }
     }
 
     private void CheckIfParameterCanBeDividedByThreeAndFive() {
+        if (!toHighChecker) return;
         if (parameter % 3 == 0 && parameter % 5 == 0) {
             System.out.println("FizzBuzz");
+            checkIfBoth = false;
         }
     }
 
     private void CheckIfParameterCantBeDividedByThreeAndFive() {
+        if (!toHighChecker) return;
         if (parameter % 3 != 0 && parameter % 5 != 0) {
             System.out.println("Die Zahl "+TerminalColors.ANSI_GREEN+ parameter +TerminalColors.ANSI_RESET+ " konnte nicht dividiert werden durch "+TerminalColors.ANSI_RED+"3 oder 5"+TerminalColors.ANSI_RESET+".");
         }
@@ -50,9 +62,9 @@ public class FizzBuzz {
     private void gameLogic() {
         System.out.println("Gib eine "+ TerminalColors.ANSI_BLUE+"Nummer"+TerminalColors.ANSI_RESET+" ein von"+TerminalColors.ANSI_RED+" 1-100"+TerminalColors.ANSI_RESET+"!");
         this.checkIfNumberToHighOrToLow();
+        this.CheckIfParameterCanBeDividedByThreeAndFive();
         this.CheckIfParameterCanBeDividedByThree();
         this.CheckIfParameterCanBeDividedByFive();
-        this.CheckIfParameterCanBeDividedByThreeAndFive();
         this.CheckIfParameterCantBeDividedByThreeAndFive();
         }
     }
