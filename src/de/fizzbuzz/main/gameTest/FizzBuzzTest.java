ff5b767d971940a29aff29c89f7f99c61dcb02a9
JeffreyHSE <Jeffrey.Ejeanye@hoffmann-group.com>
2022-10-13 16:03:42 +0200

    recoded FizzBuzzTest to adapt to new logic that I created for FizzBuzz.

--- src/de/fizzbuzz/main/gameTest/FizzBuzzTest.java
+++ src/de/fizzbuzz/main/gameTest/FizzBuzzTest.java
@@ -13,42 +13,41 @@ public class FizzBuzzTest {
 
         @Test
          public void checkIfNumberIsValid() throws Exception {
-            cut.checkIfNumberToHighOrToLow(50);
+            cut.calculate(50);
        }
 
        @Test(expected = Exception.class)
        public void checkIfNumberIsInvalid() throws Exception {
-           cut.checkIfNumberToHighOrToLow(101);
+           cut.calculate(101);
        }
 
        @Test
-        public void checkIfParameterCanBeDividedByThree() {
-           String parameter = cut.checkIfParameterCanBeDividedByThree(6);
+        public void checkIfParameterCanBeDividedByThree() throws Exception {
+           String parameter = cut.calculate(6);
            String expectedOutput = "Fizz";
            assertThat(parameter, is(expectedOutput));
        }
 
        @Test
-        public void checkIfParameterCanBeDividedByFive() {
-            String parameter = cut.checkIfParameterCanBeDividedByFive(5);
+        public void checkIfParameterCanBeDividedByFive() throws Exception {
+            String parameter = cut.calculate(5);
             String expectedOutput = "Buzz";
             assertThat(parameter, is(expectedOutput));
 
        }
 
        @Test
-        public void checkIfParameterCanBeDividedByThreeAndFive() {
-            String parameter = cut.checkIfParameterCanBeDividedByThreeAndFive(15);
+        public void checkIfParameterCanBeDividedByThreeAndFive() throws Exception {
+           String parameter = cut.calculate(15);
             String expectedOutput = "FizzBuzz";
             assertThat(parameter, is(expectedOutput));
        }
 
-
        @Test
-        public void checkIfParameterCantBeDividedByThreeAndFive() {
-           int parameter = cut.checkIfParameterCantBeDividedByThreeAndFive(7);
-           int unexpectedOutput = 0;
-           assertThat(parameter, is(unexpectedOutput));
+        public void checkIfParameterCantBeDividedByThreeAndFive() throws Exception {
+           String parameter = cut.calculate(17);
+           String expectedOutput = FizzBuzz.returnValue;
+           assertThat(parameter, is(expectedOutput));
        }
 
     }
