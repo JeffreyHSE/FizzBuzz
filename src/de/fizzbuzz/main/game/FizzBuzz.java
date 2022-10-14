b4a592b82cacae8d2f91245cdac4d79edd574de5
JeffreyHSE <Jeffrey.Ejeanye@hoffmann-group.com>
2022-10-14 11:44:56 +0200

    recoded FizzBuzz and applied new changes to it, broke down calculate methode and created a seperated class called valid. Also removed returnValue string and handled it internally.

--- src/de/fizzbuzz/main/game/FizzBuzz.java
+++ src/de/fizzbuzz/main/game/FizzBuzz.java
@@ -8,36 +8,39 @@ public class FizzBuzz {
 
     private final Scanner scanner = new Scanner(System.in);
     private int parameter = 0;
-    public static String returnValue;
 
 
     public void run() throws Exception {
         gameLogic();
     }
 
-    public String calculate(int parameter) throws Exception {
-        if (parameter > 100 || parameter < 0) {
-            System.out.println("Die Nummer "+TerminalColors.ANSI_YELLOW + parameter +TerminalColors.ANSI_RESET+ " liegt nicht zwischen"+TerminalColors.ANSI_RED+" 1-100"+TerminalColors.ANSI_RESET+"!");
-            throw new Exception("Exception");
-        }
+    public String calculate(int parameter) {
 
         if (parameter % 3 == 0 && parameter % 5 == 0) {
-            return returnValue =  "FizzBuzz";
+            return "FizzBuzz";
         }
 
         if (parameter % 3 == 0) {
-            return returnValue = "Fizz";
+            return "Fizz";
         }
 
         if (parameter % 5 == 0) {
-            return returnValue = "Buzz";
+            return "Buzz";
         }
 
-            return returnValue = Integer.toString(parameter);
+        return Integer.toString(parameter);
     }
-    private void message() {
-        if(parameter % 3 != 0 && parameter % 5 != 0) {
-            System.out.println("Die Zahl "+TerminalColors.ANSI_GREEN+ returnValue +TerminalColors.ANSI_RESET+ " konnte nicht dividiert werden durch "+TerminalColors.ANSI_RED+"3 oder 5"+TerminalColors.ANSI_RESET+".");
+
+    public void validate(int parameter) throws Exception {
+        if (parameter > 100 || parameter < 0) {
+            System.out.println("Die Nummer " + TerminalColors.ANSI_YELLOW + parameter + TerminalColors.ANSI_RESET + " liegt nicht zwischen" + TerminalColors.ANSI_RED + " 1-100" + TerminalColors.ANSI_RESET + "!");
+            throw new Exception("Exception");
+        }
+    }
+
+    private void message(String returnValue) {
+        if (parameter % 3 != 0 && parameter % 5 != 0) {
+            System.out.println("Die Zahl " + TerminalColors.ANSI_GREEN + returnValue + TerminalColors.ANSI_RESET + " konnte nicht dividiert werden durch " + TerminalColors.ANSI_RED + "3 oder 5" + TerminalColors.ANSI_RESET + ".");
         } else {
             System.out.println(returnValue);
         }
@@ -45,9 +48,11 @@ public class FizzBuzz {
 
 
     private void gameLogic() throws Exception {
-        System.out.println("Gib eine "+ TerminalColors.ANSI_BLUE+"Nummer"+TerminalColors.ANSI_RESET+" ein von"+TerminalColors.ANSI_RED+" 1-100"+TerminalColors.ANSI_RESET+"!");
+        System.out.println("Gib eine " + TerminalColors.ANSI_BLUE + "Nummer" + TerminalColors.ANSI_RESET + " ein von" + TerminalColors.ANSI_RED + " 1-100" + TerminalColors.ANSI_RESET + "!");
         parameter = Integer.parseInt(scanner.nextLine());
+        validate(parameter);
         calculate(parameter);
-        message();
-        }
+        String returnValue = calculate(parameter);
+        message(returnValue);
     }
+}
